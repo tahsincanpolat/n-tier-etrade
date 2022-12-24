faea99b0241ed4a560149774d73afb6681d45638
tahsincanpolat <tahsincanpolat@gmail.com>
2022-12-24 18:52:21 +0300

    initial

---
+++ ETrade/ETICARET.DataAccess/Concrete/EfCore/EfCoreCartDal.cs
@@ -0,0 +1,33 @@
+﻿using ETICARET.DataAccess.Abstract;
+using ETICARET.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.DataAccess.Concrete.EfCore
+{
+    public class EfCoreCartDal : EfCoreGenericRepository<Cart, DataContext>, ICartDal
+    {
+        public void ClearCart(string cartId)
+        {
+            using (var context = new DataContext())
+            {
+                var cmd = @"delete from CartItem where CartId=@p0";
+                context.Database.ExecuteSqlRaw(cmd,cartId);
+            }
+        }
+
+        public void DeleteFromCart(int cartId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cart GetCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ETrade/ETICARET.Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Image> Images { get; set; }
+        public decimal Price { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+        public List<Comment> Comments { get; set; }
+
+        public Product()
+        {
+            Images = new List<Image>();
+            ProductCategories = new List<ProductCategory>();
+            Comments = new List<Comment>();
+        }
+    }
+}
